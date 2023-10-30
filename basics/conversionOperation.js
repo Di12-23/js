3ebbcf40cbfdc7376a0aac7822f2843b92c8bedb
Di12-23 <106955977+Di12-23@users.noreply.github.com>
2023-10-30 13:56:26 +0000

    conversionOperation

---
+++ basics/conversionOperation.js
@@ -0,0 +1,12 @@
+let score="33bcd";
+console.log(typeof score);
+let valNumber = Number(score); // granteed score is Number
+console.log(typeof valNumber);
+console.log(valNumber);
+let vari = score;
+console.log(vari);
+let score1 =null;
+console.log(score1);
+// "33" => 33
+// "33bcd" => NaN
+// true => 1
