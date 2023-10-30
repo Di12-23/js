7fda400bf17db8d224458841f1ce20fc4a92d361
Di12-23 <106955977+Di12-23@users.noreply.github.com>
2023-10-30 13:25:36 +0000

    dataTypes

---
+++ basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+// alert(4+4); we are using node js not browser
+console.log(4+7)
+// code readability shold be high 
+let name = "fligh";
+let age = "20";
+let isLoggedIn =false;
+
+// Number => w to power 53
+// bigint 
+// string  prefer ""
+// boolean  true , false
+// null  standalone value
+// undefined 
+// symbol  => used for uniqueness
+
+// object
+console.log(typeof("fligh"));
+console.log(typeof null);
+console.log(typeof undefined);
