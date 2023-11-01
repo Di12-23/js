8fcd502f734bb076399249904f07b385d4ce316f
Di12-23 <106955977+Di12-23@users.noreply.github.com>
2023-11-01 04:59:43 +0000

    conversionOperatiob

---
+++ basics/03_comparision.js
@@ -0,0 +1,13 @@
+//console.log("2"=2);
+/*
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);// proplem in conversion
+// for undefined same problems occure
+console.log("2"==2);
+console.log("2"===2);  // strict check also check for data types if data types are also same than only predict true
+*/
+/*********************STRING*********************** */
+const name="IVY";
+const age=18;
+console.log(heyy this is ${name} and she is ${age} years old);
--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -1,12 +1,21 @@
 let score="33bcd";
-console.log(typeof score);
+//console.log(typeof score);
 let valNumber = Number(score); // granteed score is Number
-console.log(typeof valNumber);
-console.log(valNumber);
+//console.log(typeof valNumber);
+//console.log(valNumber);
 let vari = score;
-console.log(vari);
+//console.log(vari);
 let score1 =null;
-console.log(score1);
+//console.log(score1);
 // "33" => 33
 // "33bcd" => NaN
 // true => 1
+/******************************OPERATIONS************************** */
+let value =4;
+let neg =-value;
+console.log(neg);
+console.log(1+"2"+2);
+console.log("2"+2+2);
+console.log(1+'2'+'2');
+console.log(2+"3");
+console.log(1+2+3+"5");
